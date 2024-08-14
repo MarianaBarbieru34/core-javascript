e7f562964e7417a2ef5265623928f7ab05d5c878
MarianaBarbieru34 <126507128+MarianaBarbieru34@users.noreply.github.com>
2024-08-14 20:45:30 +0100

    exercises

--- src/challenges/02-vowels-consonants/index.js
+++ src/challenges/02-vowels-consonants/index.js
@@ -3,7 +3,7 @@ export const vowelsAndConsonants = (sentence) => {
     return "Error: Invalid sentence, should contain alphabetical characters at minimum";
   }
 
-  const sentenceArray = sentence.split("");
+  const sentenceArray = sentence.toLowerCase().split("");
 
   const hasAlphabets = sentenceArray.some((character) => {
     const alphabets = "abcdefghijklmnopqrstuvwxyz";
@@ -13,4 +13,16 @@ export const vowelsAndConsonants = (sentence) => {
   if (!hasAlphabets) {
     return "Error: Invalid sentence, should contain alphabetical characters at minimum";
   }
+
+  let counVowels = 0;
+  let countConsonants = 0;
+
+  const vowels = "aeiuo";
+  const consonants = "abcdefghijklmnopqrstuvwxyz";
+
+  sentenceArray.array.forEach((character) => {
+    if (vowels.includes(character)) {
+      counVowels = counVowels + 1;
+    }
+  });
 };
---
+++ src/exercises/arrays.js
@@ -0,0 +1,56 @@
+const numbers = [1, 2, 3];
+
+const users = ["Alice", "Bob", "Carol"];
+
+const locations = [
+  {
+    street: "London Road",
+    city: "Peterborough",
+  },
+
+  {
+    street: "Marshalls Way",
+    city: "Farcet",
+  },
+];
+
+console.log(numbers);
+
+console.log(users);
+
+console.log(locations);
+
+const doubleNumbers = numbers.map((number) => {
+  return number * 2;
+});
+
+console.log(doubleNumbers);
+
+const upperCaseUsers = users.map((user) => {
+  return user.toUpperCase();
+});
+
+console.log(upperCaseUsers);
+
+const evenNumbers = numbers.filter((number) => {
+  return number % 2 === 0;
+});
+
+console.log(evenNumbers);
+
+const oddNumbers = numbers.filter((number) => {
+  return number % 2 !== 0;
+});
+
+console.log(oddNumbers);
+
+const formattedLocations = locations.map((location) => {
+  return `${location.street}, ${location.city}`;
+});
+console.log(formattedLocations);
+
+const upperCaseLocations = locations.map((location) => {
+  return `${location.street.toUpperCase()}, ${location.city.toUpperCase()}`;
+});
+
+console.log(upperCaseLocations);
