8063544e227750e1a1bff6cf2cd83a1cddaadcc5
MarianaBarbieru34 <126507128+MarianaBarbieru34@users.noreply.github.com>
2024-08-15 16:24:54 +0100

    vowels

--- src/challenges/02-vowels-consonants/index.js
+++ src/challenges/02-vowels-consonants/index.js
@@ -1,8 +1,11 @@
 export const vowelsAndConsonants = (sentence) => {
+  // I am going to check if sentence is empty first
   if (!sentence) {
     return "Error: Invalid sentence, should contain alphabetical characters at minimum";
   }
 
+  // if not empty then I am going to convert sentence to an array
+  // don't forget to normalise sentence
   const sentenceArray = sentence.toLowerCase().split("");
 
   const hasAlphabets = sentenceArray.some((character) => {
@@ -14,15 +17,21 @@ export const vowelsAndConsonants = (sentence) => {
     return "Error: Invalid sentence, should contain alphabetical characters at minimum";
   }
 
-  let counVowels = 0;
+  let countVowels = 0;
   let countConsonants = 0;
 
   const vowels = "aeiuo";
-  const consonants = "abcdefghijklmnopqrstuvwxyz";
+  const consonants = "bcdfghjklmnpqrstvwxyz";
 
-  sentenceArray.array.forEach((character) => {
+  sentenceArray.forEach((character) => {
     if (vowels.includes(character)) {
-      counVowels = counVowels + 1;
+      countVowels = countVowels + 1;
+    }
+
+    if (consonants.includes(character)) {
+      countConsonants = countConsonants + 1;
     }
   });
+
+  return `Vowels: ${countVowels} | Consonants: ${countConsonants}`;
 };
