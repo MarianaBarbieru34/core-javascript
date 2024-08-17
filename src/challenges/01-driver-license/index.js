7a446712a6e5133b3d79977dd84e985d4233a057
MarianaBarbieru34 <126507128+MarianaBarbieru34@users.noreply.github.com>
2024-08-17 19:20:40 +0100

    driverLicense exercise and test

--- src/challenges/01-driver-license/index.js
+++ src/challenges/01-driver-license/index.js
@@ -1,12 +1,10 @@
-export const driverLicence = (age) => {
+export const driverLicense = (age) => {
   if (age <= 0 || !age || !Number.isInteger(age)) {
     return "Please enter a valid age, your age should be a positive integer.";
   }
-
   if (age >= 18 && age <= 65) {
     return "Great news, you are cleared to drive until the age of 65.";
   }
-
   if (age < 18) {
     return "Sorry, you are not allowed to drive until you turn 18 years of age.";
   }
