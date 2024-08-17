fbd0cb215689f44d7e399edc5b377b08a8ad5233
MarianaBarbieru34 <126507128+MarianaBarbieru34@users.noreply.github.com>
2024-08-17 08:11:30 +0100

    fizzBuzz

--- src/challenges/03-fizz-buzz/index.js
+++ src/challenges/03-fizz-buzz/index.js
@@ -1,1 +1,28 @@
-export const fizzBuzz = () => {};
+export const fizzBuzz = (stoppingNumber) => {
+  if (
+    !stoppingNumber ||
+    !Number.isInteger(stoppingNumber) ||
+    stoppingNumber <= 0
+  ) {
+    return "Please enter a valid ending number, should be a positive integer.";
+  }
+
+  const resultArray = [];
+
+  for (let i = 1; i <= stoppingNumber; i++) {
+    const isMultipleOf3 = i % 3 === 0;
+    const isMultipleOf5 = i % 5 === 0;
+
+    if (isMultipleOf3 && !isMultipleOf5) {
+      resultArray.push("Fizz");
+    } else if (isMultipleOf5 && !isMultipleOf3) {
+      resultArray.push("Buzz");
+    } else if (isMultipleOf3 && isMultipleOf5) {
+      resultArray.push("FizzBuzz");
+    } else {
+      resultArray.push(i);
+    }
+  }
+
+  return resultArray.join(", ");
+};
