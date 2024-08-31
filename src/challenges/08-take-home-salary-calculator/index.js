ee7b530aea5ccaa294df4171ac00463561665228
MarianaBarbieru34 <126507128+MarianaBarbieru34@users.noreply.github.com>
2024-08-31 11:59:10 +0100

    takeHomeSalary

--- src/challenges/08-take-home-salary-calculator/index.js
+++ src/challenges/08-take-home-salary-calculator/index.js
@@ -8,4 +8,55 @@ export const takeHomeSalary = (annualIncome, taxYear) => {
   if (annualIncome <= 0) {
     return "Please enter a valid salary!";
   }
+
+  //check if annualIncome =<12500
+  if (annualIncome <= 12500) {
+    return {
+      annualIncome: annualIncome,
+      taxYear: taxYear,
+      taxableIncome: 0,
+      taxPercentage: 0,
+      taxAmount: 0,
+      takeHomeSalary: annualIncome,
+    };
+  }
+
+  const taxableIncome = annualIncome - 12500;
+
+  const getTaxPercentage = (annualIncome, taxYear) => {
+    if (taxYear === "2021/2022") {
+      if (annualIncome > 12500 && annualIncome <= 40000) {
+        return 20;
+      }
+      if (annualIncome > 40000 && annualIncome <= 100000) {
+        return 40;
+      }
+      return 45;
+    }
+
+    if (taxYear === "2022/2023") {
+      if (annualIncome > 12500 && annualIncome <= 40000) {
+        return 18;
+      }
+      if (annualIncome > 40000 && annualIncome <= 100000) {
+        return 35;
+      }
+      return 40;
+    }
+  };
+
+  const taxPercentage = getTaxPercentage(annualIncome, taxYear);
+
+  const taxAmount = (taxPercentage * taxableIncome) / 100;
+
+  const takeHomeSalary = annualIncome - taxAmount;
+
+  return {
+    annualIncome: annualIncome,
+    taxYear: taxYear,
+    taxableIncome: taxableIncome,
+    taxPercentage: taxPercentage,
+    taxAmount: taxAmount,
+    takeHomeSalary: takeHomeSalary,
+  };
 };
--- src/challenges/08-take-home-salary-calculator/index.test.js
+++ src/challenges/08-take-home-salary-calculator/index.test.js
@@ -2,23 +2,124 @@ import { takeHomeSalary } from ".";
 
 describe("takeHomeSalary", () => {
   describe("for tax year 2021/2022", () => {
-    it("should return expected result for 30000", () => {});
+    it("should return expected result for 30000", () => {
+      const expected = {
+        annualIncome: 30000,
+        taxYear: "2021/2022",
+        taxableIncome: 17500,
+        taxPercentage: 20,
+        taxAmount: 3500,
+        takeHomeSalary: 26500,
+      };
 
-    it("should return expected result for 50000", () => {});
+      const actual = takeHomeSalary(30000, "2021/2022");
 
-    it("should return expected result for 150000", () => {});
+      expect(actual).toEqual(expected);
+    });
 
-    it("should return expected result for 10000", () => {});
+    it("should return expected result for 50000", () => {
+      const expected = {
+        annualIncome: 50000,
+        taxYear: "2021/2022",
+        taxableIncome: 37500,
+        taxPercentage: 40,
+        taxAmount: 15000,
+        takeHomeSalary: 35000,
+      };
+
+      const actual = takeHomeSalary(50000, "2021/2022");
+
+      expect(actual).toEqual(expected);
+    });
+
+    it("should return expected result for 150000", () => {
+      const expected = {
+        annualIncome: 150000,
+        taxYear: "2021/2022",
+        taxableIncome: 137500,
+        taxPercentage: 45,
+        taxAmount: 61875,
+        takeHomeSalary: 88125,
+      };
+
+      const actual = takeHomeSalary(150000, "2021/2022");
+
+      expect(actual).toEqual(expected);
+    });
+
+    it("should return expected result for 10000", () => {
+      const expected = {
+        annualIncome: 10000,
+        taxYear: "2021/2022",
+        taxableIncome: 0,
+        taxPercentage: 0,
+        taxAmount: 0,
+        takeHomeSalary: 10000,
+      };
+      const actual = takeHomeSalary(10000, "2021/2022");
+
+      expect(actual).toEqual(expected);
+    });
   });
 
   describe("for tax year 2022/2023", () => {
-    it("should return expected result for 30000", () => {});
+    it("should return expected result for 30000", () => {
+      const expected = {
+        annualIncome: 30000,
+        taxYear: "2022/2023",
+        taxableIncome: 17500,
+        taxPercentage: 18,
+        taxAmount: 3150,
+        takeHomeSalary: 26850,
+      };
 
-    it("should return expected result for 50000", () => {});
+      const actual = takeHomeSalary(30000, "2022/2023");
 
-    it("should return expected result for 150000", () => {});
+      expect(actual).toEqual(expected);
+    });
 
-    it("should return expected result for 10000", () => {});
+    it("should return expected result for 50000", () => {
+      const expected = {
+        annualIncome: 50000,
+        taxYear: "2022/2023",
+        taxableIncome: 37500,
+        taxPercentage: 35,
+        taxAmount: 13125,
+        takeHomeSalary: 36875,
+      };
+      const actual = takeHomeSalary(50000, "2022/2023");
+
+      expect(actual).toEqual(expected);
+    });
+
+    it("should return expected result for 150000", () => {
+      const expected = {
+        annualIncome: 150000,
+        taxYear: "2022/2023",
+        taxableIncome: 137500,
+        taxPercentage: 40,
+        taxAmount: 55000,
+        takeHomeSalary: 95000,
+      };
+
+      const actual = takeHomeSalary(150000, "2022/2023");
+
+      expect(actual).toEqual(expected);
+    });
+
+    it("should return expected result for 10000", () => {
+      const expected = {
+        annualIncome: 10000,
+        taxYear: "2022/2023",
+        taxableIncome: 0,
+        taxPercentage: 0,
+        taxAmount: 0,
+        takeHomeSalary: 10000,
+      };
+      const actual = takeHomeSalary(10000, "2022/2023");
+
+      expect(actual).toEqual(expected);
+    });
   });
 
   describe("error handling", () => {
@@ -29,7 +130,7 @@ describe("takeHomeSalary", () => {
     });
 
     it("should return error message for annual income  year 0", () => {
-      const expected = "Please enter a valid  salary!";
+      const expected = "Please enter a valid salary!";
       const actual = takeHomeSalary(0, "2021/2022");
       expect(actual).toEqual(expected);
     });
